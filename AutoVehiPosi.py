1ff67345476eec9c41242c9294a349b8c738af4b
刘恒澈 <hengchliu2-c@my.cityu.edu.hk>
2024-07-22 11:20:50 +0800

    init
    auto process vehicle data

---
+++ AutoVehiPosi.py
@@ -0,0 +1,42 @@
+import subprocess
+import os
+import argparse
+
+curDir = os.getcwd()
+
+def genSavePath(sumoPath,savePath):
+    folderName = os.path.basename(sumoPath)
+    # os.chdir(savePath)
+    # os.mkdir(folderName)
+    savePath = os.path.join(savePath,folderName)
+    if not os.path.exists(savePath):
+        os.makedirs(savePath)
+    return savePath
+
+def sumocfg2xml(sumoPath,savePath):
+    os.chdir(sumoPath)
+    xmlDir = os.path.join(savePath, "vehPosDump.xml")
+    print("the xmlDir is " + xmlDir)
+    FCDcommand = "sumo -c osm.sumocfg --netstate-dump " + xmlDir
+    subprocess.call(FCDcommand, shell=True)
+    return xmlDir
+
+def xml2csv(xmlDir):
+    os.chdir(curDir)
+    xml2csvCommand = "python xml2csv.py " + xmlDir
+    subprocess.call(xml2csvCommand, shell=True)
+
+def main(args):
+    sumoPath = args.sumoPath
+    savePath = args.savePath
+    savePath = genSavePath(sumoPath, savePath)
+    xmlDir = sumocfg2xml(sumoPath, savePath)
+    xml2csv(xmlDir)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--sumoPath', type=str, help="path to .sumocfg")
+    parser.add_argument('--savePath', type=str, help="path to save .xml & .csv")
+    args = parser.parse_args()
+    main(args)
